ecb16b5b6499438d2c63a79072d2a8d4b52c6c13
marknjoroge <marknnjorogeh@gmail.com>
2021-10-09 01:58:10 +0300

    routing set up

--- index.py
+++ index.py
@@ -2,13 +2,42 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+products = {
+    "resistance-band": [
+        "../static/images/logo.png",
+        "../static/images/logo.png",
+        "../static/images/logo.png",
+        "../static/images/logo.png",
+        "../static/images/logo.png",
+        "../static/images/logo.png",
+        "../static/images/logo.png",
+        "../static/images/logo.png",
+    ],
+    "foam-roller-set": [
+        "../static/images/logo.png",
+        "../static/images/foamrollerset.webp",
+        "../static/images/foamrollerset.webp",
+        "../static/images/foamrollerset.webp",
+        "../static/images/foamrollerset.webp",
+        "../static/images/foamrollerset.webp",
+        "../static/images/foamrollerset.webp",
+        "../static/images/logo.png",
+    ],
+}
+
+
 @app.route("/")
 def home():
     return render_template("index.html")
 
+
 @app.route("/<product>")
 def product(product):
-    return f"this product"
+    for key, value in products.items():
+        if key == product:
+            return render_template("buy.html", product=product, images=value)
+    return render_template("notfound.html")
+
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="localhost", port=8000, debug=True)
